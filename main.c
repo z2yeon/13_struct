401682745248dfc7a42742cbb8d15b78fe06edbd
z2yeon <55427811+z2yeon@users.noreply.github.com>
2019-11-26 15:33:58 +0900

    13_01

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 struct student{
@@ -11,6 +11,13 @@ struct student{
 
 int main(int argc, char *argv[]) {
 	struct student s1={24,"Yeonsu",4.3};
+	s1.ID=123456l;
+	strcpy(s1.name,"Yoonseo");
+	s1.grade=4.0;
+	
+	printf("ID: %d\n",s1.ID);
+	printf("name: %s\n",s1.name);
+	printf("grade: %f\n",s1.grade);
 	
 	return 0;
 }
